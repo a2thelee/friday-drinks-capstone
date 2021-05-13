d7429604d70d45db5e2fe6c5e8d1e311f67e7810
Andrew Lee <andrewyujaylee@gmail.com>
2021-05-13 14:00:22 -0400

    started drink ingredient route

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -43,7 +43,7 @@ const LoginForm = () => {
         <div>
           <label htmlFor="email">Email</label>
           <input
-            classname="login-input"
+            className="login-input"
             name="email"
             type="text"
             placeholder="Email"
