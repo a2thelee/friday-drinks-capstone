b13418ba3cb449cc0d250e7bc75d058f877b9404
Andrew Lee <andrewyujaylee@gmail.com>
2021-05-14 23:04:03 -0400

    got ingredients displaying but still needs keys on each ingredient made AND to allow clicks on only stuff inside database

--- react-app/src/components/CreateDrinkForm/CreateDrinkForm.js
+++ react-app/src/components/CreateDrinkForm/CreateDrinkForm.js
@@ -19,6 +19,7 @@ function CreateDrinkForm() {
 
   //an array to keep track of submitted ingredients since we are only using one input
   const [ingredients, setIngredients] = useState([])
+  const [ingredientNames, setIngredientNames] = useState([])
 
   useEffect(() => {
     dispatch(getIngredientsThunk())
@@ -45,78 +46,93 @@ function CreateDrinkForm() {
   //ingredient submission handler. sends ingredient IDs to thunk to make querying easier on the back end. WORKS!
   const ingredientSubmit = (e) => {
     e.preventDefault();
+    ingredientNames.push(ingredient)
     setIngredients([...ingredients, +e.currentTarget.dataset.ingredientId])
     setIngredient("")
   }
 
+  // const displayIngredients = (e) => {
+  //   setIngredientNames([...ingredient])
+  // }
+
   return (
-    <div className="create-drink-div">
-      <form onSubmit={handleSubmit} className="create-drink-form">
-        <div className="drink-name-div">
-          <label>Drink Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </div>
+    <>
+      <div className="user-ingredients-list">
+        <p>Active Ingredients</p>
+        {ingredientNames.map(ingredient => {
+          return (
+            <p>{ingredient}</p>
+          )
+        })}
+      </div>
+      <div className="create-drink-div">
+        <form onSubmit={handleSubmit} className="create-drink-form">
+          <div className="drink-name-div">
+            <label>Drink Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
 
-        <div className="checkbox-div">
-          <label> Contains Alcohol? </label>
-          <input
-            type="checkbox"
-            checked={isAlcoholic}
-            onChange={checkAlcoholic}
-          />
-        </div>
+          <div className="checkbox-div">
+            <label> Contains Alcohol? </label>
+            <input
+              type="checkbox"
+              checked={isAlcoholic}
+              onChange={checkAlcoholic}
+            />
+          </div>
 
-        <div className="drink-picture-div">
-          <label>Drink Picture</label>
-          <input
-            type="file"
-            onChange={addPhoto}
-          />
-        </div>
+          <div className="drink-picture-div">
+            <label>Drink Picture</label>
+            <input
+              type="file"
+              onChange={addPhoto}
+            />
+          </div>
 
-        <div className="ingredients-input-div">
-          <label>Ingredients</label>
-          <input
-            type="text"
-            placeholder="Gin"
-            value={ingredient}
-            onChange={e => setIngredient(e.target.value)}
-          />
+          <div className="ingredients-input-div">
+            <label>Ingredients</label>
+            <input
+              type="text"
+              placeholder="Gin"
+              value={ingredient}
+              onChange={e => setIngredient(e.target.value)}
+            />
 
-          {ingredientsList.filter((item) => {
-            if (ingredient === "") {
-              return ""
-            } else if (item.name.toLowerCase().includes(ingredient.toLowerCase())) {
-              return item
-            }
-          }).map((ingredient) => {
-            return (
-              <div data-ingredient-id={ingredient.id}
-                key={ingredient.id}
-                className="ingredients-input-list"
-                onClick={ingredientSubmit}>
-                <p>{ingredient.name}</p>
-              </div>
-            )
-          })}
-        </div>
+            {ingredientsList.filter((item) => {
+              if (ingredient === "") {
+                return ""
+              } else if (item.name.toLowerCase().includes(ingredient.toLowerCase())) {
+                return item
+              }
+            }).map((ingredient) => {
+              return (
+                <div data-ingredient-id={ingredient.id}
+                  key={ingredient.id}
+                  className="ingredients-input-list"
+                  onClick={ingredientSubmit}>
+                  <p>{ingredient.name}</p>
+                </div>
+              )
+            })}
+          </div>
 
-        <div className="instructions-div">
-          <label>Instructions</label>
-          <textarea
-            type="text"
-            placeholder="measurements and directions"
-            required
-            value={instructions}
-            onChange={e => setInstructions(e.target.value)} />
-        </div>
-      </form>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </div >
+          <div className="instructions-div">
+            <label>Instructions</label>
+            <textarea
+              type="text"
+              placeholder="measurements and directions"
+              required
+              value={instructions}
+              onChange={e => setInstructions(e.target.value)} />
+          </div>
+        </form>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </div >
+    </>
   )
 
 }
