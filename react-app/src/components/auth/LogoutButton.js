335735a96c6d8ac4777a0d04936179316bce3615
Andrew Lee <andrewyujaylee@gmail.com>
2021-05-23 17:29:10 -0400

    redirects to home when logging out

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
+import { useHistory } from "react-router-dom"
 
 import "./logout.css"
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
+  const history = useHistory()
   const onLogout = async (e) => {
-    await dispatch(logout());
+    await history.push("/")
+    dispatch(logout());
   };
 
   return <button className="logout-button" onClick={onLogout}>Logout</button>;
