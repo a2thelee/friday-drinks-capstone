84d4cf90abf26897fcd75675e2cf8d15acc516e5
Andrew Lee <andrewyujaylee@gmail.com>
2021-06-08 17:42:12 -0400

    added some info for future look up and edits

--- react-app/src/components/drinkCard/DrinkCard.js
+++ react-app/src/components/drinkCard/DrinkCard.js
@@ -1,3 +1,5 @@
+//this is modal component
+
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHistory } from "react-router-dom"
