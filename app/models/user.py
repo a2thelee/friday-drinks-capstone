7bbb295383f8acb29065bed744a48e5b7841067c
Andrew Lee <andrewyujaylee@gmail.com>
2021-06-22 13:56:45 -0400

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -31,5 +31,6 @@ class User(db.Model, UserMixin):
     return {
       "id": self.id,
       "username": self.username,
-      "email": self.email
+      "email": self.email,
+      "favorites": [favorite_drink.to_dict() for favorite_drink in self.favorite_drink]
     }
